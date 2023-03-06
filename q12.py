e75a8c7016c1260333ca29df20d3d3196dd542ec
kubra-buber <69219486+kubra-buber@users.noreply.github.com>
2023-03-07 00:48:00 +0300

    Update q12.py

--- q12.py
+++ q12.py
@@ -10,15 +10,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-
-df = pd.read_csv("pi_intern_dataset.csv")
+#take the data
+df = pd.read_csv("pi_intern_dataset.csv") 
 
 f1 = df["feature_1"]
 f2 = df["feature_2"]
 f3 = df["feature_3"]
 f4 = df["feature_4"]
 virus = df["isVirus"]
-
+#visuale the data with different wiev planes
 plt.figure(figsize = (11, 5), dpi = 80)
 plt.scatter(f1, f2, c = virus, cmap = plt.cool(), label = "Virus")
 plt.legend(loc = 'upper right', fontsize = 15)
@@ -79,6 +79,7 @@ plt.xlabel("f3", fontsize = 20)
 plt.ylabel("f4", fontsize = 20)
 plt.show()
 
+# get rid of raw with empty inputs
 df = df.dropna().reset_index(drop = True)
 
 f1 = df["feature_1"]
@@ -87,6 +88,8 @@ f3 = df["feature_3"]
 f4 = df["feature_4"]
 virus = df["isVirus"]
 
+#visualize new data (without empty inputs)
+
 plt.figure(figsize = (11, 5), dpi = 80)
 plt.scatter(f1, f2, c = virus, cmap = plt.cool(), label = "Virus")
 plt.legend(loc = 'upper right', fontsize = 15)
@@ -146,6 +149,8 @@ plt.ylim(-8, 10)
 plt.xlabel("f3", fontsize = 20)
 plt.ylabel("f4", fontsize = 20)
 plt.show()
+
+#create data sets for ML model
 
 X = []
 for i in range(0, len(df)):
@@ -158,10 +163,16 @@ for i in range(0, len(df)):
 X = np.array(X)
 Y = np.array(Y)
 
+#slice data sets for train and test sets
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 1, stratify = Y)
+
+#train the model
 
 model = LogisticRegression(solver = 'liblinear', random_state = 0)
 model.fit(X_train, Y_train)
+
+#evaluate the model
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -171,6 +182,9 @@ X_pred = model.predict(X_test)
 print(model.intercept_, model.coef_, model.score(X_train, Y_train))
 print(model.intercept_, model.coef_, model.score(X_test, Y_test))
 
+# score is 0.657 for train set and 0.687 for test set that shows the accuracy of the model. It is good enough but may be improved by using different methods.
+
+#confusion matrix 
 cm = confusion_matrix(Y_test, model.predict(X_test))
 
 fig, ax = plt.subplots(figsize=(6,6))
@@ -183,6 +197,8 @@ for i in range(2):
     for j in range(2):
         ax.text(j, i, cm[i, j], ha = 'center', va ='center', color = 'red')
 plt.show()
+#confusion matrix shows the model good at true negatives, others are close to eachs other
 
+#classification report
 print(classification_report(Y_test, model.predict(X_test)))
 print(classification_report(Y_train, model.predict(X_train)))
