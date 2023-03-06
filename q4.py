3fab36dac5622e7bfe3b7dbf6ab8d9bfede39bd3
kubra-buber <69219486+kubra-buber@users.noreply.github.com>
2023-03-06 23:26:42 +0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,15 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import scipy
-import statistics
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import StandardScaler
-from sklearn import metrics
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("country_vaccination_stats.csv")
 
